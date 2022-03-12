740faa0cf64d8c95ea339f0e2ceb663ecb2f36d3
Siddharth Jain <vasujain8998@gmail.com>
2022-03-12 12:11:15 +0530

    add

--- rockpapsci.js
+++ rockpapsci.js
@@ -1,5 +1,6 @@
 // Randomly return either 'Rock', 'Paper' or 'Scissors'.
 
 function computerPlay() {
-    
+    let randoutput = ["Rock", "Paper", "Scissors"];
+    return randoutput[Math.floor(Math.random() * 3)];
 }
