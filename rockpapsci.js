d5fb8594684fb90d9cc0bab1caa66ba87177e7c8
Siddharth Jain <vasujain8998@gmail.com>
2022-03-12 19:52:13 +0530

    add

--- rockpapsci.js
+++ rockpapsci.js
@@ -33,39 +33,53 @@ let compScore = 0;
 
 // Variable to store Player Score
 let playerScore = 0;
-
+let roundNo = 0;
 // Function to Define Rules of each Round
 function playRound(playerSelection,computerSelection){
     if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+        roundNo++;
+        console.log("Round: " +roundNo);
         console.log("Your Choice: "+ playerSelection + ", " + "Computer: " + computerSelection);
         console.log("It's a draw!");
         console.log("Your Score: " + playerScore +", " +"Computer Score: " + compScore);
     } else if (playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'paper') {
+        roundNo++;
+        console.log("Round: " +roundNo);
         console.log("Your Choice: "+ playerSelection + ", " + "Computer: " + computerSelection);
         console.log("Paper beats Rock, You Lose!");
         compScore++;
         console.log("Your Score: " + playerScore +", " +"Computer Score: " + compScore);
     } else if (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'rock') {
+        roundNo++;
+        console.log("Round: " +roundNo);
         console.log("Your Choice: "+ playerSelection + ", " + "Computer: " + computerSelection);
         console.log("Paper beats Rock, You Won!");
         playerScore++;
         console.log("Your Score: " + playerScore +", " +"Computer Score: " + compScore);
     } else if (playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'scissors') {
+        roundNo++;
+        console.log("Round: " +roundNo);
         console.log("Your Choice: "+ playerSelection + ", " + "Computer: " + computerSelection);
         console.log("Rock beats Scissors, You Won!");
         playerScore++;
         console.log("Your Score: " + playerScore +", " +"Computer Score: " + compScore);
     } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'rock') {
+        roundNo++;
+        console.log("Round: " +roundNo);
         console.log("Your Choice: "+ playerSelection + ", " + "Computer: " + computerSelection);
         console.log("Rock beats Scissors, You lose!");
         compScore++;
         console.log("Your Score: " + playerScore +", " +"Computer Score: " + compScore);
     } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'paper') {
+        roundNo++;
+        console.log("Round: " +roundNo);
         console.log("Your Choice: "+ playerSelection + ", " + "Computer: " + computerSelection);
         console.log("Scissors beats Paper, You Won!");
         playerScore++;
         console.log("Your Score: " + playerScore +", " +"Computer Score: " + compScore);
     } else if (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'scissors') {
+        roundNo++;
+        console.log("Round: " +roundNo);
         console.log("Your Choice: "+ playerSelection + ", " + "Computer: " + computerSelection);
         console.log("Scissors beats Paper, You Lose!");
         compScore++;
@@ -85,11 +99,11 @@ game();
 // Function to Declare Winner
 function winner() {
     if(playerScore === compScore) {
-        console.log("It was a Close Game!!");
+        console.log("Result\nIt was a Close Game!!");
     } else if (playerScore > compScore) {
-        console.log("You won this Game!!");
+        console.log("Result\nYou won this Game!!");
     } else {
-        console.log("You Lost this Game!!");
+        console.log("Result\nYou Lost this Game!!");
     }
 }
 winner();
