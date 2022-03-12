b416ae628408cce171049164075513706c8679b6
Siddharth Jain <vasujain8998@gmail.com>
2022-03-12 12:47:31 +0530

    add

--- rockpapsci.js
+++ rockpapsci.js
@@ -1,6 +1,18 @@
-// Randomly return either 'Rock', 'Paper' or 'Scissors'.
+let userinput = prompt("Rock, Paper or Scissors ?");
+let playerSelection = userinput.toLowerCase();
 
-function computerPlay() {
-    let randoutput = ["Rock", "Paper", "Scissors"];
-    return randoutput[Math.floor(Math.random() * 3)];
+// Randomly return either 'Rock', 'Paper' or 'Scissors'.
+let randoutput = ["Rock", "Paper", "Scissors"];
+let computerSelection = randoutput[Math.floor(Math.random() * 3)];
+
+// function that plays a single round of Rock Paper Scissors.
+console.log(computerSelection)
+function startGame() {
+    if (playerSelection == "rock" && computerSelection === "Paper") {
+        console.log('You lose!');
+    } else if (playerSelection == "rock" && computerSelection === "Rock") {
+        console.log('Tie!');
+    } else {
+        console.log('You won!')
+    }
 }
